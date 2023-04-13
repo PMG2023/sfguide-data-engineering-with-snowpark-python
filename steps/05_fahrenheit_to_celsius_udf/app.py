8a952418ba9c4fbdf43eda7f7a9e55158883e739
PMG2023 <129332967+PMG2023@users.noreply.github.com>
2023-04-13 18:46:41 +0000

    added scipy

--- steps/05_fahrenheit_to_celsius_udf/app.py
+++ steps/05_fahrenheit_to_celsius_udf/app.py
@@ -20,6 +20,9 @@ from scipy.constants import convert_temperature
 def main(temp_f: float) -> float:
     return convert_temperature(float(temp_f), 'F', 'C')
 
+def main(temp_f: float) -> float:
+    return convert_temperature(float(temp_f), 'F', 'C')
+
 # For local debugging
 # Be aware you may need to type-convert arguments if you add input parameters
 if __name__ == '__main__':
